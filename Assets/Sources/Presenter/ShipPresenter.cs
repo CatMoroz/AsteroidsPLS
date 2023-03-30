b6d7531e849869e2c1495cd7eca27f2c85b33288
CatMoroz <116497717+CatMoroz@users.noreply.github.com>
2023-03-30 19:54:05 +0300

    AdteroidS

---
+++ Assets/Sources/Presenter/ShipPresenter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ShipPresenter : Presenter
+{
+    private Root _init;
+
+    public void Init(Root init)
+    {
+        _init = init;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            if (_init.Ship.Heathe>1)
+            {
+                _init.Ship.Heathe--;
+            }
+            else
+            {
+                _init.Ship.Heathe--;
+                _init.Ship.Destroy();
+                _init.DisableShip();
+            }
+        }
+    }
+}
